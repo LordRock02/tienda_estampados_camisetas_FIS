bf982111ffca5ced61d77ce48e1983f8d671d75b
Roger Gómez Torres <rogergt02@gmail.com>
2023-10-25 03:36:49 -0500

    creada base de datos funcional para crear usuarios e iniciar sesion

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,24 @@
-from flask import Blueprint, render_template, request, flash
-from .models import User, db
+from flask import Blueprint, redirect, render_template, request, flash, url_for
+from .models import User, User_type, db
 from werkzeug.security import generate_password_hash, check_password_hash
+from passlib.hash import sha256_crypt
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/sign-in', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        userName = request.form.get('userName')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=userName).first()
+        if user:
+            if password == user.password:
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('User does not exist.', category='error')
     return render_template("sign_in.html", boolean=True)
 
 @auth.route('/logout')
@@ -19,7 +32,11 @@ def sign_up():
         email=request.form.get('email')
         password=request.form.get('password')
         password2=request.form.get('password2')
-        if password!=password2:
+
+        user = User.query.filter_by(username=userName).first()
+        if user:
+            flash('Email already exists', category='error')
+        elif password!=password2:
             flash('The passwords don\'t match', category='error')
         elif password == '' or password2 == '':
             flash('Please introduce a password', category='error')
@@ -28,9 +45,9 @@ def sign_up():
         elif email == '':
             flash('Please introduce an E-mail', category='error')
         else:
-            new_user = User(userName = userName, email = email, password = password)
+            new_user = User(username = userName, email = email, password = password, userType =1)
             db.session.add(new_user)
             db.session.commit()
             flash('Account created', category='succes')
-    
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -3,16 +3,16 @@ from . import db
 from sqlalchemy.sql import func
 #modelos de las tablas de la base de datos
 
+class User_type(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20), nullable=False)
+    users = db.relationship('User')
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    date_added = db.Column(db.DateTime, default=func.now)
-    UserType = db.Column(db.Integer, db.ForeignKey('UserType.id'))
-class UserType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(20), nullable=False)
+    userType = db.Column(db.Integer, db.ForeignKey('user_type.id'))
 """class T_Shirt(db.Model):
     pass
 class Stamp(db.Model):
