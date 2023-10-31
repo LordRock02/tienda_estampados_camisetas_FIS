e60a20dcac90086ff2402551f690e0a7d057230f
Filomaticom <dmmontoyag@udistrital.edu.co>
2023-10-30 22:41:06 -0500

    Vistas prints y tshirts

--- website/views.py
+++ website/views.py
@@ -5,3 +5,12 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/prints')
+def prints():
+    return render_template("prints.html")
+
+@views.route('/tshirts')
+def tshirts():
+    return render_template("t-shirts.html")
+
