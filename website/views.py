203e882749f15faea653cc3d217d9c17d0c9b026
Roger Gómez Torres <rogergt02@gmail.com>
2023-11-14 09:20:21 -0500

    agregadas categorias a upload_print.html

--- website/views.py
+++ website/views.py
@@ -23,25 +23,17 @@ def design_details():
     return render_template('print_details.html')
 @views.route('/upload-design', methods=['GET', 'POST'])
 def uploadDesign():
-    cateogries = get_categories()
-    list = ['video games', 'anime', 'sports', 'music']
-    
+    categories = Category.query.all()
     if request.method=='POST':
+        print('HOla')
         uploaded_print = request.files.get('print')
         if uploaded_print.filename!='':
             filename = secure_filename(uploaded_print.filename)
             file_path = os.path.join(UPLOAD_FOLDER, filename)
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
             uploaded_print.save(file_path)
-            
         #return redirect(url_for('views.design_details'))
-    return render_template("upload_print.html", cateogries=cateogries)
-def get_categories():
-    """
-    get a list of categories from de data base
-
-    Return : 
-    a list of categories
-    """
-    categories=Category.query.all()
-    return categories
+        selected_categories = request.form.getlist('selected_categories[]')
+        for category in selected_categories:
+            print(f"Selected category: {category}")
+    return render_template("upload_print.html", categories=categories)
